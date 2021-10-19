395a82cd5501f8a3b15b9a4b0e18ecb2ddcb63af
Nishant-Goutam <nishu70555@gmail.com>
2021-10-19 15:11:02 +0530

    hi

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Item = (props) => {
-    const dragStart = e => {
+    const dragStart = e =>{
         const target = e.target;
         e.dataTransfer.setData('id', target.id)
 
